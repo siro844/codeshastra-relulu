308503b47383586fa1a494ae5ec714c3f55346fb
surabhiwaingankar <surabhiwaingankar@gmail.com>
2024-03-30 13:35:44 +0530

    added gmail agent

---
+++ backend/agents/gmail_agent.py
@@ -0,0 +1,20 @@
+from langchain.agents.agent_toolkits import GmailToolkit
+
+toolkit = GmailToolkit() 
+
+import os
+os.environ['OPENAI_API_KEY'] = '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'
+
+from langchain import OpenAI
+from langchain.agents import initialize_agent, AgentType
+
+llm = OpenAI(temperature=0)
+
+agent = initialize_agent(
+    tools=toolkit.get_tools(),
+    llm=llm,
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+print(agent.run("Send a email to srinathreddy239@gmil.com sih team mein aayega kya"))
+
